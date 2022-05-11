bfc297f09bfac15d151213490fe5c7901ce06bcf
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-05-11 20:46:59 +0200

    fertig

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+
+function certainNumber(x) {
+    if (x > 27) {
+        // return (x - 27) * 2;
+        console.log((x - 27) * 2);
+    } else {
+        console.log(x - 27)
+        // return x - 27;
+    }
+}
+
+certainNumber(35);
+certainNumber(74);
+certainNumber(123);
